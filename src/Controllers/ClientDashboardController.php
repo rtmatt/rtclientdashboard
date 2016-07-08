1fc3889ce3a733c372c5338a7a14bd64b33953c6
Matt Emrick <matt@redtrainwebservices.com>
2016-07-08 14:25:53 -0600

    Added test functionality to dashboard

--- src/Controllers/ClientDashboardController.php
+++ src/Controllers/ClientDashboardController.php
@@ -11,4 +11,19 @@ use RTMatt\MonthlyServiceClientDashboard\Traits\ShowsDashboard;
 
 class ClientDashboardController extends Controller{
    use ShowsDashboard;
+    public function test($client_id){
+
+        $rt_client = \RTMatt\MonthlyService\Client::find($client_id);
+        $key       = $rt_client->generateApiKey();
+        $username  = $key->api_name;
+        $password  = \Hash::make($key->api_secret_key);
+        $client   = new Client([
+            'base_uri' => 'http://dashboard.dev',
+        ]);
+        $response = $client->request('GET', route('client-services-summary'), [ 'auth' => [ $username, $password ] ]);
+        $dashboard_data     = json_decode($response->getBody()->getContents());
+        $auth = base64_encode("{$username}:{$password}");
+
+        return view('rtdashboard::dashboard', compact('dashboard_data','rt_client','client_id','auth'));
+    }
 }
