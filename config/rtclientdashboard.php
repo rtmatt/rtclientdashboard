7b1fa59bb4b3d7518bf01a29d3d7fd97c9222eba
Matt Emrick <matt@redtrainwebservices.com>
2016-07-15 21:35:42 -0600

    updated summary endpoint

--- config/rtclientdashboard.php
+++ config/rtclientdashboard.php
@@ -1,5 +1,5 @@
 <?php
 return [
     'api_base_url'=> env('DASHBOARD_API_URL','http://dashboard.dev'),
-    'api_summary_endpoint'=>env('DASHBOARD_SUMMARY_API_ENDPOINT','/api/client-service')
+    'api_summary_endpoint'=>env('DASHBOARD_SUMMARY_API_ENDPOINT','/api/client-service/summary')
 ];
